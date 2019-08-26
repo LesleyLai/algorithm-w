7035eacb0153b077a16bd28cb1a13f444c176a5e
Lesley Lai <lesley@lesleylai.info>
2019-08-25 20:28:00 -0600

    Pretty print for values

--- Program.fs
+++ Program.fs
@@ -2,7 +2,42 @@
 
 open System
 
+type Literal = EInt of int | EString of String | EBool of bool
+type Expr = EIdentifier of String
+          | ELiteral of Literal
+          | EApp of Expr * Expr
+          | ELet of String * Expr * Expr
+
+type Type = TVar of String
+          | TBool
+          | TInt
+          | TString
+          | TFun of Type * Type
+
+type Scheme = Scheme of Type
+
+
+type Printer =
+    static member Show(x:Literal) =
+        match x with
+            | EInt i -> sprintf "%A" i
+            | EString s -> sprintf "%A" s
+            | EBool b -> sprintf "%A" b
+        
+    static member Show(x:Expr) =
+        match x with
+            | EIdentifier id -> id
+            | ELiteral lit -> Printer.Show lit
+            | EApp (f, x) -> sprintf "(%s %s)" (Printer.Show f) (Printer.Show x)
+            | ELet (name, a, b) ->
+                sprintf "(let %s=%s;\n %s)" name (Printer.Show(a)) (Printer.Show(b))
+            
+    static member Show(x:Type) = printf "%A" x
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.WriteLine (Printer.Show(
+                           ELet ("x", ELiteral (EInt 1),
+                               EApp (EIdentifier "f", ELiteral (EBool true)))))
     0 // return an integer exit code
